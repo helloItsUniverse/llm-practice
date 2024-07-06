5abc65e8b9f1a8828267439b796c46d9261a821a
helloItsUniverse <hello.its.universe@gmail.com>
2024-07-06 11:14:03 +0900

    text_cls model change

--- text_cls.py
+++ text_cls.py
@@ -2,10 +2,12 @@
 from transformers import pipeline
 
 # STEP 2: Load model
-classifier = pipeline("sentiment-analysis", model="stevhliu/my_awesome_model")
+# classifier = pipeline("sentiment-analysis", model="stevhliu/my_awesome_model")
+classifier = pipeline("sentiment-analysis", model="snunlp/KR-FinBert-SC")
 
 # STEP 3: Input data
-text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+# text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+text = "C쇼크에 멈춘 흑자비행…대한항공 1분기 영업적자 566억"
 
 # STEP 4: Run Inference
 result = classifier(text)
